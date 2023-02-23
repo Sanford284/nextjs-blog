7443362ec144156520439da8f97369f15d4dbab0
yuwanlin <yuwanlin@cvte.com>
2023-02-23 11:46:10 +0800

    add revalidate

--- pages/posts/[...id].tsx
+++ pages/posts/[...id].tsx
@@ -3,6 +3,7 @@ import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
+import { GetStaticProps } from 'next';
 
 export default function Post({ postData }) {
     return (
@@ -29,11 +30,12 @@ export async function getStaticPaths() {
     };
 }
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
     const postData = await getPostData(params.id);
     return {
         props: {
             postData,
         },
+        revalidate: 10,
     };
-}
+};
